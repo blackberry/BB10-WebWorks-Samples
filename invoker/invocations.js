6acdc151f3310e787c19d712205ed8338920e763
Hasan Ahmad <haahmad@rim.com>
2012-06-25 17:01:03 -0400

    Updated invocations to automatically load a PNG file to shared/downloads that will be used to invoke Pictures

--- invoker/invocations.js
+++ invoker/invocations.js
@@ -50,7 +50,71 @@ function invokeApp() {
 }
 
 function invokePictures() {
+    
+    downloadPicture();
+    
     blackberry.invoke.invoke({
-        uri: "file:///accounts/1000/shared/downloads/HTML5.png",
+        uri: "file:///accounts/1000/shared/downloads/HTML5_Logo_512.png",
     }, onSuccess, onError);
+}
+
+function downloadPicture() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "/HTML5_Logo_512.png", true);
+    xhr.responseType = 'arraybuffer';
+    
+    xhr.onload = function(e) {
+        if (this.status == 200) {
+            var bb = new window.WebKitBlobBuilder();
+            bb.append(this.response);
+            var blob = bb.getBlob('image/png');
+            saveFile(blob);
+        }
+    };
+    xhr.send();
+}
+
+function saveFile (blob) {
+    function gotFs(fs) {
+        fs.root.getFile("/accounts/1000/shared/downloads/HTML5_Logo_512.png", {create: true}, gotFile, errorHandler);
+    }
+
+    function gotFile(fileEntry) {
+        fileEntry.createWriter(gotWriter, errorHandler);
+    }
+
+    function gotWriter(fileWriter) {
+        fileWriter.onerror = function (e) {
+            alert("Failed to write PNG: " + e.toString());
+        }
+        fileWriter.write(blob);
+    }
+    window.webkitRequestFileSystem(PERSISTENT, 10 * 1024, gotFs, errorHandler);
+}
+
+function errorHandler(e) {
+    var msg = '';
+
+    switch (e.code) {
+        case FileError.QUOTA_EXCEEDED_ERR:
+            msg = 'QUOTA_EXCEEDED_ERR';
+            break;
+        case FileError.NOT_FOUND_ERR:
+            msg = 'NOT_FOUND_ERR';
+            break;
+        case FileError.SECURITY_ERR:
+            msg = 'SECURITY_ERR';
+            break;
+        case FileError.INVALID_MODIFICATION_ERR:
+            msg = 'INVALID_MODIFICATION_ERR';
+            break;
+        case FileError.INVALID_STATE_ERR:
+            msg = 'INVALID_STATE_ERR';
+            break;
+        default:
+            msg = 'Unknown Error';
+          break;
+    };
+
+    alert('Error: ' + msg);
 }
