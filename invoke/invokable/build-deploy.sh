193995a2a5c7987b4fb4c80b5537a2762c9030c0
Hasan Ahmad <haahmad@rim.com>
2012-07-10 17:15:41 -0400

    Removed build scripts

--- invoke/invokable/build-deploy.sh
+++
@@ -1,4 +0,0 @@
-rm -rf device/ simulator/ invokable.zip
-zip invokable.zip * -r
-~/Documents/ci/packager4/bbwp -d invokable.zip
-blackberry-deploy -installapp -launchapp -device 169.254.0.1 -password qaqa -package device/invokable.bar
--- invoke/invoker/build-deploy.sh
+++
@@ -1,4 +0,0 @@
-rm -rf device/ simulator/ invoker.zip
-zip invoker.zip * -r
-~/Documents/ci/packager4/bbwp -d invoker.zip
-blackberry-deploy -installapp -launchapp -device 169.254.0.1 -password qaqa -package device/invoker.bar
