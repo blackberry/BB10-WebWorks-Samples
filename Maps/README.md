6accfcf3882578b605aa0742daeb800f104da0d0
Chad Tetreault <ctetreault@rim.com>
2012-11-21 14:24:48 -0500

    initial commit

---
+++ Maps/README.md
@@ -0,0 +1,112 @@
+# Maps Sample
+
+This sample demonstrates how to integrate a few popular mapping services into a BlackBerry WebWorks application for BlackBerry 10.
+
+The sample code for this application is Open Source under the [Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0.html).
+
+
+**Applies To**
+
+* [BlackBerry 10 WebWorks SDK](https://developer.blackberry.com/html5/download/sdk) 
+
+**Author(s)** 
+
+* Chad Tetreault (http://www.twitter.com/chadtatro)
+
+**Dependencies**
+
+1. [jquery-1.7.2.js](http://code.jquery.com/jquery-1.7.2.js) is [dual licensed](http://jquery.org/license/) under the MIT or GPL Version 2 licenses.
+
+2. [bbUI.js] (https://github.com/blackberry/bbUI.js) is [licensed] (https://github.com/blackberry/bbUI.js/blob/master/LICENSE) under the Apache 2.0 license.
+
+**Icons**<br/>
+Icons used here are from [http://subway.pixle.pl/rim](http://subway.pixle.pl/rim) are [licensed](http://creativecommons.org/licenses/by/3.0/) under the CC-BY-3.0 license.  This is a subset of the Subway icons available at http://subway.pixle.pl/
+
+**Demo Video**<br/>
+Check-out the demo video on the [BlackBerry DevTube Channel](http://www.youtube.com/watch?v=lSn3rex7PCI).
+
+
+**To contribute code to this repository you must be [signed up as an official contributor](http://blackberry.github.com/howToContribute.html).**
+
+## Initial Setup
+
+***To use this sample, you must sign up for API keys from the service you wish to use.  The links below will point you in the right direction. ***
+
+[Google Maps](https://code.google.com/apis/console)<br/>
+[Bing Maps](http://www.bingmapsportal.com/)<br/>
+[Leaflet via CloudMade](http://ww.cloudmade.com/)<br/>
+[OpenLayers](http://www.openlayers.org)<br/>
+
+## Add your API Keys
+Open ***js/maps.js*** and enter your API Keys in the proper place
+
+```
+function initApp() {
+
+  APIKey = {
+    'bing': '<my bing key>',
+    'leaflet': '<my leafleft/cloudmade key>'
+  };
+
+  console.log('app initialized');
+  startGeolocation();
+}
+```
+
+## WebWorks App Setup
+1. Open index.html
+2. Include the webworks-.js file ***see How To Build*** for instructions
+
+
+## Config.xml 
+As of BlackBerry WebWorks 1.0.2.9 SDK, all domains you plan on making Ajax/XHR requests to must be whitelisted in your app's config.xml.
+
+```
+<access uri="*" subdomains="true" />
+<access uri="http://virtualearth.net" subdomains="true" />
+<access uri="http://bing.net" subdomains="true" />
+```
+
+Don't forget to access the devices location information via HTML5 Geolocation, the proper permissions must be requested.
+
+```
+<rim:permissions>  
+   <rim:permit>read_geolocation</rim:permit>
+</rim:permissions>
+```
+
+## Security Considerations
+
+Your API Keys are meant to be private. For demonstration purposes we're coding the keys right in the JavaScript source.  This is not best practice, and is not recommended. You don’t want anybody to get access to your keys.
+
+***One way*** to securely pass your API Keys to your application is to host them on a server, then use SSL and do a POST to obtain your keys. 
+
+
+## How to Build
+
+1. Clone the repo to your local machine
+2. Copy the webworks-.js client file from C:\Program Files\Research In Motion\BlackBerry 10 WebWorks SDK \Framework\clientFiles (Windows) or ~/SDKs/BlackBerry 10 WebWorks SDK /Framework/clientFiles (Mac) into the top level of the local Facebook-OAuth-2 folder (e.g. ~/WebWorks/Facebook-OAuth-2/js/webworks/webworks-.js).
+3. Optional: Edit the script reference to the webworks-.js file within any *.html files to ensure the version number is correct.
+4. Use Using the Ripple Mobile Emulator and either the BlackBerry WebWorks SDK for Smartphone or the BlackBerry WebWorks SDK for Tablet OS, package the contents of your local BB10-WebWorks-Samples/Facebook-OAuth-2 folder into a BlackBerry application.
+
+## More Info
+
+* [BlackBerry HTML5 WebWorks](https://bdsc.webapps.blackberry.com/html5/) - Downloads, Getting Started guides, samples, code signing keys.
+* [BlackBerry WebWorks Development Guides](https://bdsc.webapps.blackberry.com/html5/documentation)
+* [BlackBerry WebWorks Community Forums](http://supportforums.blackberry.com/t5/Web-and-WebWorks-Development/bd-p/browser_dev)
+* [BlackBerry Open Source WebWorks Contributions Forums](http://supportforums.blackberry.com/t5/BlackBerry-WebWorks/bd-p/ww_con)
+
+
+## Contributing Changes
+
+Please see the [README](https://github.com/blackberry/BB10-WebWorks-Samples) of the BB10-WebWorks-Samples repository for instructions on how to add new Samples or make modifications to existing Samples.
+
+
+## Bug Reporting and Feature Requests
+
+If you find a bug in a Sample, or have an enhancement request, simply file an [Issue](https://github.com/blackberry/BB10-WebWorks-Samples/issues) for the Sample and send a message (via github messages) to the Sample Author(s) to let them know that you have filed an [Issue](https://github.com/blackberry/BB10-WebWorks-Samples/issues).
+
+## Disclaimer
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ Maps/js/maps.js
@@ -0,0 +1,263 @@
+/*
+* Copyright 2010-2011 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ *  called by the webworksready event when the environment is ready
+ */
+function initApp() {
+  APIKey = {
+    'bing': '',
+    'leaflet': ''
+  };
+
+  console.log('app initialized');
+  startGeolocation();
+}
+
+
+/**
+ *  google maps
+ */
+
+// initialize the map
+function initGoogleMaps() {
+  myLocation = new google.maps.LatLng(myLat, myLong);
+
+  var mapOptions = {
+    zoom: 14,
+    center: myLocation,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoomControl: false,
+    mapTypeControl: false,
+    streetViewControl: false
+  };
+  googleMap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
+
+// search for nearby places
+function initGooglePlaces() {
+  searchForPlaces(googlePlacesCallback);
+}
+
+// search callback
+function googlePlacesCallback(results) {
+  for(var i = 0; i < results.length; i++) {
+    createGoogleMarker(results[i]);
+  }
+}
+
+// create a marker / push-pin
+function createGoogleMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: googleMap,
+    position: place.geometry.location
+  });
+}
+
+
+
+/**
+ *  bing
+ */
+
+// initialize the map
+function initBingMaps() {
+  setTimeout(function() {
+    bingMap = new Microsoft.Maps.Map(document.getElementById("map_canvas"), {
+      credentials: APIKey.bing,
+      center: new Microsoft.Maps.Location(myLat, myLong),
+      zoom: 14,
+      mapTypeId: Microsoft.Maps.MapTypeId.road,
+      showCopyright: false
+    });
+  }, 200);
+}
+
+// search for nearby places
+function initBingPlaces() {
+  Microsoft.Maps.loadModule('Microsoft.Maps.Search', {
+    callback: bingSearch
+  });
+}
+
+// seraching bing, using a query
+function bingSearch() {
+  bingMap.addComponent('searchManager', new Microsoft.Maps.Search.SearchManager(bingMap));
+  searchManager = bingMap.getComponent('searchManager');
+
+  var query = 'bars in Waterloo, Canada';
+  var request = {
+    query: query,
+    count: 15,
+    startIndex: 0,
+    bounds: bingMap.getBounds(),
+    callback: bingPlacesCallback
+  };
+  searchManager.search(request);
+}
+
+// search callback
+function bingPlacesCallback(result) {
+  var searchResults = result && result.searchResults;
+  if(searchResults) {
+    for(var i = 0; i < searchResults.length; i++) {
+      createBingMarker(searchResults[i]);
+    }
+  }
+}
+
+// create marker / push-pin
+function createBingMarker(result) {
+  if(result) {
+    var pin = new Microsoft.Maps.Pushpin(result.location, null);
+    bingMap.entities.push(pin);
+  }
+}
+
+
+
+/**
+ *  leaflet
+ */
+
+// initialize the map
+function initLeafletMaps() {
+  leafletMap = L.map('map_canvas').setView([myLat, myLong], 14);
+  L.tileLayer('http://{s}.tile.cloudmade.com/' + APIKey.leaflet + '/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 16
+  }).addTo(leafletMap);
+}
+
+// search for nearby places
+function initLeafletPlaces() {
+  searchForPlaces(leafletPlacesCallback);
+}
+
+// search callback
+function leafletPlacesCallback(results) {
+  for(var i = 0; i < results.length; i++) {
+    var lat = results[i].geometry.location.Ya;
+    var lon = results[i].geometry.location.Za;
+    var name = results[i].name;
+    var address = results[i].vicinity;
+
+    // add marker
+    var marker = L.marker([lat, lon]).addTo(leafletMap);
+  }
+}
+
+
+
+/**
+ *  openlayers
+ */
+
+// initialize the map
+function initOpenLayersMaps() {
+  openLayersMap = new OpenLayers.Map('map_canvas');
+  layer = new OpenLayers.Layer.OSM("Simple OSM Map");
+  openLayersMap.addLayer(layer);
+  openLayersMap.setCenter(
+    new OpenLayers.LonLat(myLong, myLat).transform(
+    new OpenLayers.Projection("EPSG:4326"), openLayersMap.getProjectionObject()
+  ), 14);
+}
+
+// search for nearby places
+function initOpenLayersPlaces() {
+  searchForPlaces(openLayersPlacesCallback);
+}
+
+// search callback
+function openLayersPlacesCallback(results) {
+  if(results) {
+    openLayersOverlay = new OpenLayers.Layer.Vector('Overlay', {
+      styleMap: new OpenLayers.StyleMap({
+        externalGraphic: 'http://www.openlayers.org/dev/img/marker.png',
+        graphicWidth: 20,
+        graphicHeight: 24,
+        graphicYOffset: -24,
+        title: '${tooltip}'
+      })
+    });
+    openLayersMap.addLayer(openLayersOverlay);
+    createOpenLayersMarker(results);
+  }
+}
+
+// create marker / push-pin
+function createOpenLayersMarker(results) {
+    for(var i = 0; i < results.length; i++) {
+      var lat = results[i].geometry.location.Ya;
+      var lon = results[i].geometry.location.Za;
+      var myLocation = new OpenLayers.Geometry.Point(lon, lat).transform('EPSG:4326', 'EPSG:3857');
+      openLayersOverlay.addFeatures([
+      new OpenLayers.Feature.Vector(myLocation)]);
+    }
+}
+
+
+
+/**
+ *  search for POI
+ */
+
+// example of seraching for places using google
+function searchForPlaces(callback) {
+  var request = {
+    location: myLocation,
+    radius: 2000,
+    types: ['bar']
+  };
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(googleMap);
+  service.search(request, callback);
+  return;
+}
+
+
+
+/**
+ *  geolocation
+ */
+
+// we use HTML5 Geolocation to pin-point where the user is
+function startGeolocation() {
+  var options;
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoFail, options);
+}
+
+// geolocation success callback
+function geoSuccess(position) {
+  var gpsPosition = position;
+  var coordinates = gpsPosition.coords;
+  myLat = coordinates.latitude;
+  myLong = coordinates.longitude;
+  bb.pushScreen('google.html', 'google');
+}
+
+// geolocation failure callback
+function geoFail() {
+  alert('Error getting your position. Using defaults instead');
+
+  // set default position upon failure
+  myLat = 43.465187;
+  myLong = -80.522372;
+  bb.pushScreen('google.html', 'google');
+}
