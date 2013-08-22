4eed18084e281f1f40fda940a534bb466c23c7ea
Erik Oros <eoros@rim.com>
2013-08-22 15:09:47 -0400

    Leaflet key must be uppercase.

--- Maps/js/maps.js
+++ Maps/js/maps.js
@@ -137,7 +137,7 @@ function createBingMarker(result) {
 // initialize the map
 function initLeafletMaps() {
   leafletMap = L.map('map_canvas').setView([myLat, myLong], 14);
-  L.tileLayer('http://{s}.tile.cloudmade.com/' + APIKey.leaflet + '/997/256/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tile.cloudmade.com/' + APIKey.leaflet.toUpperCase() + '/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 16
   }).addTo(leafletMap);
