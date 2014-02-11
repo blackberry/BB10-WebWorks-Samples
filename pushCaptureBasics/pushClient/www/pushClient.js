0fc8b12ec21432ec54836254c9823c65a6216eb3
oros <eoros@blackberry.com>
2014-02-11 14:44:52 -0500

    Updated license company name.

--- pushCaptureBasics/pushClient/www/pushClient.js
+++ pushCaptureBasics/pushClient/www/pushClient.js
@@ -1,7 +1,7 @@
 /*global window, FileReader, blackberry, autoExit, utils */
 
 /*
-* Copyright 2014 Research In Motion Limited.
+* Copyright 2014 BlackBerry Limited.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
